2e15c906b952aa09568c27d4f053257082a9406d
xpr <a.mameter@proton.me>
2025-03-25 18:10:36 -0400

    oh

--- home-manager/distrobox.nix
+++ home-manager/distrobox.nix
@@ -1,66 +1,66 @@
-{
-  pkgs,
-  config,
-  ...
-}: {
-  imports = [./modules/distrobox.nix];
+{pkgs, ...}: let
+  inherit (builtins) concatStringsSep filter typeOf;
+  inherit (import ../scripts pkgs) box;
+  nvim = import ../nvim {inherit pkgs;};
 
-  programs.distrobox = {
-    enable = true;
+  mkBox = name: {
+    image,
+    exec ? "nu",
+    packages ? [],
+  }: let
+    distropkgs = concatStringsSep " " (filter
+      (p: typeOf p == "string")
+      (packages ++ ["wl-clipboard" "git"]));
 
-    boxes = let
-      exec = "${pkgs.nushell}/bin/nu";
-      symlinks = [
-        ".bashrc"
-        ".zshrc"
-        ".config/nushell"
-        ".config/nvim"
-        ".config/nix"
-        ".config/starship.toml"
-      ];
-      packages =
-        config.packages.cli
-        ++ [
-          config.programs.neovim.finalPackage
-          pkgs.nix
-          pkgs.git
-        ];
-    in {
-      Alpine = {
-        inherit exec symlinks;
-        img = "docker.io/library/alpine:latest";
-      };
-      Fedora = {
-        inherit exec symlinks;
-        packages = "nodejs npm poetry gcc mysql-devel python3-devel wl-clipboard";
-        img = "registry.fedoraproject.org/fedora-toolbox:rawhide";
-        nixPackages =
-          packages
-          ++ [
-            (pkgs.writeShellScriptBin "pr" "poetry run $@")
-            (pkgs.writeShellScriptBin "prpm" "poetry run python manage.py $@")
-          ];
-      };
-      Arch = {
-        inherit exec symlinks;
-        img = "docker.io/library/archlinux:latest";
-        packages = "base-devel wl-clipboard";
-        nixPackages =
-          packages
-          ++ [
-            (pkgs.writeShellScriptBin "yay" ''
-              if [[ ! -f /bin/yay ]]; then
-                tmpdir="$HOME/.yay-bin"
-                if [[ -d "$tmpdir" ]]; then sudo rm -r "$tmpdir"; fi
-                git clone https://aur.archlinux.org/yay-bin.git "$tmpdir"
-                cd "$tmpdir"
-                makepkg -si
-                sudo rm -r "$tmpdir"
-              fi
-              /bin/yay $@
-            '')
-          ];
-      };
-    };
-  };
+    path =
+      [
+        "/bin"
+        "/sbin"
+        "/usr/bin"
+        "/usr/sbin"
+        "/usr/local/bin"
+        "$HOME/.local/bin"
+      ]
+      ++ ["${nvim}/bin" "${pkgs.nushell}/bin"]
+      ++ (map (p: "${p}/bin") (filter (p: typeOf p == "set") packages));
+
+    db-exec = pkgs.writeShellScript "db-exec" ''
+      export XDG_DATA_DIRS="/usr/share:/usr/local/share"
+      export PATH="${builtins.concatStringsSep ":" path}"
+      if [ $# -eq 0 ]; then ${exec}; else bash -c "$@"; fi
+    '';
+  in
+    pkgs.writeShellScriptBin name ''
+      ${box} ${name} ${image} ${db-exec} $@ --pkgs "${distropkgs}"
+    '';
+in {
+  home.packages = [
+    pkgs.distrobox
+    (mkBox "ubuntu" {
+      image = "quay.io/toolbx/ubuntu-toolbox:latest";
+    })
+    (mkBox "fedora" {
+      image = "registry.fedoraproject.org/fedora-toolbox:rawhide";
+      packages = ["gcc poetry python-devel mysql-devel pango-devel nodejs npm cargo" pkgs.lazygit];
+    })
+    (mkBox "alpine" {
+      image = "docker.io/library/alpine:latest";
+    })
+    (mkBox "arch" {
+      image = "docker.io/library/alpine:latest";
+      packages = let
+        yay = pkgs.writeShellScriptBin "yay" ''
+          if [[ ! -f /bin/yay ]]; then
+            tmpdir="$HOME/.yay-bin"
+            if [[ -d "$tmpdir" ]]; then sudo rm -r "$tmpdir"; fi
+            git clone https://aur.archlinux.org/yay-bin.git "$tmpdir"
+            cd "$tmpdir"
+            makepkg -si
+            sudo rm -r "$tmpdir"
+          fi
+          /bin/yay $@
+        '';
+      in ["base-devel" yay];
+    })
+  ];
 }
