8cf07664c791c72d910cc6156d61504ff8280d3c
xpr <168950241+xprshh@users.noreply.github.com>
2025-03-26 08:30:23 -0400

    Update theme.nix

--- home-manager/theme.nix
+++ home-manager/theme.nix
@@ -1,16 +1,26 @@
 {
   pkgs,
   config,
-  inputs,
   ...
 }: let
+  nerdfonts = pkgs.nerdfonts.override {
+    fonts = [
+      "Ubuntu"
+      "UbuntuMono"
+      "CascadiaCode"
+      "FantasqueSansMono"
+      "FiraCode"
+      "Mononoki"
+    ];
+  };
+
   theme = {
     name = "adw-gtk3-dark";
     package = pkgs.adw-gtk3;
   };
   font = {
     name = "Ubuntu Nerd Font";
-    package = pkgs.nerd-fonts.ubuntu;
+    package = nerdfonts;
     size = 11;
   };
   cursorTheme = {
@@ -20,38 +30,58 @@
   };
   iconTheme = {
     name = "MoreWaita";
-    package = pkgs.morewaita-icon-theme.overrideAttrs {
-      src = inputs.morewaita;
-    };
+    package = pkgs.morewaita-icon-theme;
   };
 in {
   home = {
     packages = with pkgs; [
+      cantarell-fonts
+      font-awesome
       theme.package
       font.package
       cursorTheme.package
       iconTheme.package
-      nerd-fonts.caskaydia-cove
+      adwaita-icon-theme
+      papirus-icon-theme
     ];
-    # sessionVariables = {
-    #   XCURSOR_THEME = cursorTheme.name;
-    #   XCURSOR_SIZE = "${toString cursorTheme.size}";
-    # };
-    # pointerCursor = cursorTheme // {gtk.enable = true;};
+    sessionVariables = {
+      XCURSOR_THEME = cursorTheme.name;
+      XCURSOR_SIZE = "${toString cursorTheme.size}";
+    };
+    pointerCursor =
+      cursorTheme
+      // {
+        gtk.enable = true;
+      };
+    file = {
+      ".config/gtk-4.0/gtk.css".text = ''
+        window.messagedialog .response-area > button,
+        window.dialog.message .dialog-action-area > button,
+        .background.csd{
+          border-radius: 0;
+        }
+      '';
+    };
   };
 
   fonts.fontconfig.enable = true;
 
-  # gtk = {
-  #   inherit font cursorTheme iconTheme;
-  #   theme.name = theme.name;
-  #   enable = true;
-  # };
-  #
-  # qt = {
-  #   enable = true;
-  #   platformTheme.name = "kde";
-  # };
+  gtk = {
+    inherit font cursorTheme iconTheme;
+    theme.name = theme.name;
+    enable = true;
+    gtk3.extraCss = ''
+      headerbar, .titlebar,
+      .csd:not(.popup):not(tooltip):not(messagedialog) decoration{
+        border-radius: 0;
+      }
+    '';
+  };
+
+  qt = {
+    enable = true;
+    platformTheme.name = "kde";
+  };
 
   home.file.".local/share/flatpak/overrides/global".text = let
     dirs = [
