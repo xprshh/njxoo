ae8fffbc66f38a47dd1788f03a6402362c93223b
xpr <a.mameter@proton.me>
2025-03-25 21:52:45 -0400

    o

--- nixos/nixovo.nix
+++ nixos/nixovo.nix
@@ -4,25 +4,6 @@
   # Enable thermald for better thermal management
   services.thermald.enable = true;
 
-  boot.kernelParams = [
-    "intel_pstate=passive"
-    "pcie_aspm=force"
-    "usbcore.autosuspend=1"
-    "acpi_osi=Linux"
-    "acpi_backlight=vendor"
-    "mem_sleep_default=deep"
-    "intel_idle.max_cstate=9"  # Check highest C-state for your CPU
-    "rcu_nocbs=0-7"  # Change 0-7 based on `nproc` output
-    "nmi_watchdog=0"
-    "i915.enable_fbc=1"
-    "i915.enable_psr=1"
-    "i915.enable_rc6=3"  # 3 is max documented value
-  ];
-
-  # Enable Bluetooth
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.powerOnBoot = false;  # Saves power by default
-
   # Enable periodic trimming for Ext4 SSD
   services.fstrim.enable = true;
 
@@ -44,7 +25,7 @@
 
   # Install power-saving utilities
   environment.systemPackages = with pkgs; [
-    powertop
+    powertop`
     acpi
     pciutils
     ethtool
