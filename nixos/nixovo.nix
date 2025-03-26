4e23f6b2541dda237576cdfd935732a57a1dca2a
xpr <a.mameter@proton.me>
2025-03-25 21:47:42 -0400

    ;o

--- nixos/nixovo.nix
+++ nixos/nixovo.nix
@@ -1,6 +1,7 @@
 { config, pkgs, lib, ... }:
 
 {
+  # Enable thermald for better thermal management
   services.thermald.enable = true;
 
   boot.kernelParams = [
@@ -10,19 +11,19 @@
     "acpi_osi=Linux"
     "acpi_backlight=vendor"
     "mem_sleep_default=deep"
-    "intel_idle.max_cstate=9"
-    "rcu_nocbs=0-$(nproc)"
+    "intel_idle.max_cstate=9"  # Check highest C-state for your CPU
+    "rcu_nocbs=0-7"  # Change 0-7 based on `nproc` output
     "nmi_watchdog=0"
     "i915.enable_fbc=1"
     "i915.enable_psr=1"
-    "i915.enable_rc6=7"
+    "i915.enable_rc6=3"  # 3 is max documented value
   ];
 
   # Enable Bluetooth
   hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = false;  # Saves power by default
 
-
-  # Enable periodic trimming of unused disk blocks
+  # Enable periodic trimming for Ext4 SSD
   services.fstrim.enable = true;
 
   systemd.sleep.extraConfig = ''
@@ -30,13 +31,18 @@
     HibernateState=disk
   '';
 
+  # Kernel optimizations
   boot.kernel.sysctl = {
     "vm.laptop_mode" = 10;
     "vm.dirty_writeback_centisecs" = 3000;
   };
 
+  # Enable X11 server (if using Xorg)
   services.xserver.enable = true;
-  services.xserver.videoDrivers = [ "intel" ];
+  services.xserver.videoDrivers = [ "intel" ];  # Remove if using Wayland
+
+  # Enable TLP for better battery life
+  services.tlp.enable = true;
 
   # Install power-saving utilities
   environment.systemPackages = with pkgs; [
@@ -47,10 +53,4 @@
     wayland-utils
     lm_sensors
   ];
-
-  # Example:
-  # lenovo = {
-  #   enable = true;
-  # };
-
 }
